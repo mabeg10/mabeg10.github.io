c8c53a8bfea0aacb5841eab0499735a9575e84b5
mbguzman.099@gmail.com <mabe@192.168.1.5>
2024-08-29 12:03:36 -0500

    chore: add skeleton for portfolio and initial views

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+// main.js
+
+const translations = {
+    en: {
+        home: 'Home',
+        projects: 'Projects',
+        skills: 'Skills',
+        about: 'I am a Fullstack Developer specializing in HTML, CSS, and JavaScript, with a background in Educommunication, art, and culture. I am passionate about creating and optimizing interactive websites by merging technology with my expertise in graphic design and social media. My top priority is to ensure that the user experience is both intuitive and functional. I adapt swiftly and excel in collaborative settings. My continuous commitment to learning and professional growth drives me to develop innovative and creative solutions for the tech industry.'
+    },
+    es: {
+        home: 'Inicio',
+        projects: 'Proyectos',
+        skills: 'Habilidades',
+        about: 'Soy una Desarrolladora Fullstack con especialización en HTML, CSS y JavaScript, tengo una formación previa en Educomunicación, arte y cultura. Me apasiona crear y optimizar sitios web interactivos combinando la tecnología con mi experiencia en artes gráficas y redes sociales. Garantizar que la experiencia del usuario sea intuitiva y funcional es mi prioridad. Me adapto rápidamente y soy eficaz trabajando en espacios colaborativos. Mi compromiso constante con el aprendizaje y el desarrollo profesional me motiva a crear soluciones innovadoras y creativas para el mundo de la tecnología.'
+    }
+};
+
+function setLanguage(language) {
+    document.querySelectorAll('[data-translate]').forEach(element => {
+        const key = element.getAttribute('data-translate');
+        element.textContent = translations[language][key] || element.textContent;
+    });
+
+    localStorage.setItem('language', language);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const savedLanguage = localStorage.getItem('language') || 'en'; // Default to English
+    document.getElementById('language-select').value = savedLanguage;
+    setLanguage(savedLanguage);
+});
---
+++ scripts/views.js
@@ -0,0 +1,59 @@
+// Traducciones generales para todos los elementos
+const translations = {
+    en: {
+        home: 'Home',
+        projectsTitle: 'Projects',
+        projects:'Projects',
+        textAnalyzer: 'Text Analyzer',
+        dataverseChat: 'Dataverse Chat',
+        skillsTitle: 'Skills',
+        description: 'Description: An e-commerce website built for small businesses to sell products online. The website features a user-friendly interface, shopping cart functionality, and secure payment processing.',
+        technologiesUsed: 'Technologies Used: HTML, CSS, JavaScript',
+        liveProject: 'Live Project',
+        githubRepository: 'GitHub Repository',
+        screenshotDescription: 'Screenshot Gilmore Girls Serie',
+        contact: 'Contact'    
+    },
+    es: {
+        home: 'Inicio',
+        projectsTitle: 'Proyectos',
+        projects:'Proyectos',
+        textAnalyzer: 'Analizador de Texto',
+        dataverseChat: 'Dataverse Chat',
+        skillsTitle: 'Habilidades',
+        description: 'Descripción: Aplicación web de análisis de texto que proporciona métricas detalladas sobre la estructura y el contenido de un texto. La herramienta permite ingresar un texto y ofrece en tiempo real datos como el recuento de palabras, caracteres, la suma total de números, y la longitud media de las palabras.',
+        technologiesUsed: 'Tecnologías usadas: HTML, CSS, JavaScript',
+        liveProject: 'Página del Proyecto',
+        githubRepository: 'Repositorio en GitHub',
+        screenshotDescription: 'Captura de Pantalla Analizador de Texto',
+        contact: 'Contacto'
+    }
+};
+
+// Función para aplicar traducciones
+function setLanguage(language) {
+    document.querySelectorAll('[data-translate]').forEach(element => {
+        const key = element.getAttribute('data-translate');
+        if (translations[language][key]) {
+            element.textContent = translations[language][key];
+        }
+    });
+
+    // Para el título de la página
+    const titleElement = document.querySelector('title');
+    if (titleElement) {
+        const pageTitleKey = titleElement.getAttribute('data-translate');
+        if (translations[language][pageTitleKey]) {
+            titleElement.textContent = translations[language][pageTitleKey];
+        }
+    }
+
+    localStorage.setItem('language', language);
+}
+
+// Inicializar el idioma al cargar la página
+document.addEventListener('DOMContentLoaded', () => {
+    const savedLanguage = localStorage.getItem('language') || 'en'; // Idioma por defecto
+    document.getElementById('language-select').value = savedLanguage;
+    setLanguage(savedLanguage);
+});
