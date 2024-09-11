9b84dc4dafa6a9ddebeb855cc8581ee82defbc0c
mbguzman.099@gmail.com <mabe@192.168.1.28>
2024-09-11 12:10:35 -0500

    fix(styles): adjust right margin for profile picture on mobile devices

--- scripts/main.js
+++ scripts/main.js
@@ -4,13 +4,15 @@ const translations = {
         home: 'Home',
         projects: 'Projects',
         skills: 'Skills',
-        about: "I'm a Fullstack Developer specializing in HTML, CSS, and JavaScript, with a background in Educommunication, art, and culture. I'm passionate about creating and optimizing interactive websites by merging technology with my expertise in graphic design and social media. Ensuring that the user experience is intuitive and functional is my priority. I'm quick to adapt and effective working in collaborative spaces. My constant commitment to learning and professional development motivates me to create innovative and creative solutions for the technology world"
+        about: "I'm a Fullstack Developer specializing in HTML, CSS, and JavaScript, with a background in Educommunication, art, and culture. I'm passionate about creating and optimizing interactive websites by merging technology with my expertise in graphic design and social media. Ensuring that the user experience is intuitive and functional is my priority. I'm quick to adapt and effective working in collaborative spaces. My constant commitment to learning and professional development motivates me to create innovative and creative solutions for the technology world",
+        contact: 'Contact: mbguzman.099@gmail.com',
     },
     es: {
         home: 'Inicio',
         projects: 'Proyectos',
         skills: 'Habilidades',
-        about: 'Soy una Desarrolladora Fullstack con especialización en HTML, CSS y JavaScript, tengo una formación previa en Educomunicación, arte y cultura. Me apasiona crear y optimizar sitios web interactivos combinando la tecnología con mi experiencia en artes gráficas y redes sociales. Garantizar que la experiencia del usuario sea intuitiva y funcional es mi prioridad. Me adapto rápidamente y soy eficaz trabajando en espacios colaborativos. Mi compromiso constante con el aprendizaje y el desarrollo profesional me motiva a crear soluciones innovadoras y creativas para el mundo de la tecnología.'
+        about: 'Soy una Desarrolladora Fullstack con especialización en HTML, CSS y JavaScript, tengo una formación previa en Educomunicación, arte y cultura. Me apasiona crear y optimizar sitios web interactivos combinando la tecnología con mi experiencia en artes gráficas y redes sociales. Garantizar que la experiencia del usuario sea intuitiva y funcional es mi prioridad. Me adapto rápidamente y soy eficaz trabajando en espacios colaborativos. Mi compromiso constante con el aprendizaje y el desarrollo profesional me motiva a crear soluciones innovadoras y creativas para el mundo de la tecnología.',
+        contact: 'Contacto: mbguzman.099@gmail.com',
     }
 };
 
